06742c752415b31fa4fec6b11f6c01997406aaab
Lucas wu <wsx1531913990@hotmail.com>
2024-02-22 20:42:10 +0800

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@ from playwright.sync_api import Page
 @pytest.fixture(scope="function", autouse=True)
 def goto_radio_page(page: Page):
     print('The beginning of the test')
-    page.goto("https://iviewui.com/")
-    page.get_by_role("link", name="体验免费组件库").click()
-    page.locator("div").filter(has_text=re.compile(r"^组件$")).click()
-    page.locator("div").filter(has_text=re.compile(r"^表单$")).click()
-    page.get_by_role("link", name="Radio 单选框").click()
+    page.goto("http://iviewui.com/")
+    page.get_by_role("link", name="Getting Started").click()
+    page.locator("div").filter(has_text=re.compile(r"Component")).click()
+    page.locator("div").filter(has_text=re.compile(r"Form")).click()
+    page.get_by_role("link", name="Radio").click()
     yield page
     print('The end of the test')
--- tests/myTest02.py
+++ tests/myTest02.py
@@ -4,8 +4,8 @@ from playwright.sync_api import expect
 class Test02:
 
     def test_goto_radio_page(self, page) -> None:
-        expect(page.get_by_role("heading", name="Radio 单选框")).to_be_visible()
+        expect(page.get_by_role("heading", name="Radio")).to_be_visible()
 
     def test_select_windows_radio(self, page) -> None:
-        page.get_by_role("radio", name=" Windows").check()
-        page.get_by_role("radio", name=" Windows").is_checked()
+        page.get_by_role("radio", name="Windows").check()
+        page.get_by_role("radio", name="Windows").is_checked()
